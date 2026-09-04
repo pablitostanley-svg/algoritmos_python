6f7fd1a8da60791b4c0b1cfce8878e430faa2aa6
Pablo <pablitostanley@hotmail.com>
2026-09-04 20:05:07 -0300

    criacao do projeto e algoritmo media

---
+++ index.py
@@ -0,0 +1,10 @@
+n1 = float (input('Primeira nota:'))
+n2 = float (input('Segunda nota:'))
+n3 = float (input('Terceira nota:'))
+media = (n1+n2+n3) / 3
+print (f'Média: {media:.2f}')
+if media >7=:
+    print ('Aprovado!')
+    elif media >=5:
+        print('Recuperação!')
+        else: print('Reprovado')
