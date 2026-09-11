01afa40c85fa7bef0119459b59235be1b3a89188
Pablo <pablitostanley@hotmail.com>
2026-09-11 19:24:38 -0300

    criando subtracao

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+print("### CALCULADORA PYTHON ###")
+
+ def adicao (num1,num2):
+    soma= num1 + num2
+    return soma
+
+    print,("Resultado da soma:", adicao(16,9))
+  
+  def subtracao (num1,num2):
+    resultado = num1 - num2
+    return resultado
+
+    print,("Resultado da subtração:", subtracao(89,33))
